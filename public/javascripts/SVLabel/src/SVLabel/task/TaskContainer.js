792bc53bd36abe31f2a8cfcc7f678affbd1d8fd7
Sage Chen <sagec108@gmail.com>
2017-07-06 10:25:53 -0400

    Changed sign in prompt to show at 250m

--- public/javascripts/SVLabel/src/SVLabel/task/TaskContainer.js
+++ public/javascripts/SVLabel/src/SVLabel/task/TaskContainer.js
@@ -95,7 +95,7 @@ function TaskContainer (navigationModel, neighborhoodModel, streetViewService, s
         updateAuditedDistance("miles");
 
         if (!('user' in svl) || (svl.user.getProperty('username') == "anonymous" &&
-            getCompletedTaskDistance(neighborhood.getProperty("regionId"), "kilometers") > 0.15 &&
+            getCompletedTaskDistance(neighborhood.getProperty("regionId"), "kilometers") > 0.25 &&
             !svl.popUpMessage.haveAskedToSignIn())) {
             svl.popUpMessage.promptSignIn();
         }
